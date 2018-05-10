113f2621a2af0f7304f41f3a069842fca7ca2bad
Dasel <daselopez@gmail.com>
2018-05-10 16:05:13 -0500

    chat-videostream-loging twitter

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root to:"home#index"
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
+  mount ActionCable.server => "/cable"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
